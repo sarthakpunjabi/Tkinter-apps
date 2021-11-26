1550d5997b896fbdecf71928bb2c79be0530bc4f
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2021-11-26 04:57:42 +0000

    working with json files

---
+++ project/password-manager/main2.py
@@ -0,0 +1,125 @@
+""" This application manages your password and profile """
+import random
+from tkinter import Tk,Canvas,Label,Button,Entry,END,messagebox
+from PIL import Image,ImageTk
+import pyperclip
+import json
+
+def generate_password():
+    """
+    This module automatically generates password
+    """
+    small_letters = [chr(x) for x in range(ord('a'),ord('z')+1)]
+    big_letters = [chr(x) for x in range(ord('A'),ord('Z')+1)]
+    letters = small_letters + big_letters
+    symbols = [chr(x) for x in range(ord('!'),ord('/')+1)]
+    numbers = [chr(x) for x in range(ord('0'),ord('9')+1)]
+    password = ""
+    strong_password=""
+    for i in range(0,10):
+        password += random.choice(letters)+" "
+
+    for i in range(0,4):
+        password += random.choice(symbols)+" "
+
+    for i in range(0,5):
+        password += random.choice(numbers)+" "
+
+    password = password.split(" ")
+    password.pop()
+    random.shuffle(password)
+    for i in range(0,len(password)):
+        strong_password += password[i]
+
+    pyperclip.copy(strong_password)
+    passwordinp.insert(0,string=f"{strong_password}")
+
+def save():
+    """
+    This module is to add data into database
+    """
+    def clean():
+        """
+        This module is to clean Entry after storing data
+        """
+        webinp.delete(0,END)
+        passwordinp.delete(0,END)
+        username.delete(0,END)
+        messagebox.showinfo(title="save",message="Your data got saved")
+
+    def validator():
+        """
+        It helps validating the Entry
+        """
+        if len(webinp.get())==0 or len(username.get())==0 or len(passwordinp.get())==0:
+            return False
+        else:
+            return True
+    
+    new_data = {
+                f"{webinp.get()}":{
+                    "email":f"{username.get()}",
+                    "password":f"{passwordinp.get()}"
+                }
+            }
+    try:        
+        if validator():
+            print("trying")
+            with open("database.json",mode="r",encoding='utf-8') as fil:
+                data = json.load(fil)
+    
+    except:
+        print("Exception")
+        messagebox.showwarning(title="warning message",message=f"")
+        with open("database.json",mode="w",encoding="utf-8") as fill:
+            if validator():
+                fill.seek(0)
+                json.dump(new_data,fill,indent=4)
+                clean()
+    
+    else:
+        print("in else")
+        data.update(new_data)
+        with open("database.json",mode="w",encoding='utf-8') as fil:
+            if validator():
+                json.dump(data,fil,indent=4)
+                clean()
+
+#screen
+window = Tk()
+window.title("Password manager")
+window.geometry('500x500')
+window.config(padx=50,pady=50,bg="black")
+
+#cavas
+canvas = Canvas(width=200,height=200,highlightthickness=0,bg='black')
+img = ImageTk.PhotoImage(Image.open("project/password-manager/logo.gif"))
+image = canvas.create_image(100,100,image=img)
+canvas.grid(row=0,column=1)
+
+#Labels
+website = Label(window,text="Website:",bg="black",fg="white")
+website.grid(row=1,column=0)
+webid = Label(window,text="Username/email",bg="black",fg="white")
+webid.grid(row=2,column=0)
+passwords = Label(window,text="Password",bg="black",fg="white")
+passwords.grid(row=3,column=0)
+
+#Input
+webinp = Entry(width=35,bg="black",fg="white",highlightthickness=0)
+webinp.focus()
+webinp.grid(row=1,column=1,columnspan=2,pady=10)
+username = Entry(width=35,bg="black",fg="white",highlightthickness=0)
+username.insert(END,string="Enter User Id")
+username.grid(row=2,column=1,columnspan=2,pady=10)
+passwordinp = Entry(width=22,bg="black",fg="white",highlightthickness=0)
+passwordinp.insert(END,string="Enter Password")
+passwordinp.grid(row=3,column=1,pady=20)
+
+#button
+generate = Button(width=8,highlightbackground='red',text='Generate',command=generate_password)
+generate.grid(row=3,column=2)
+add = Button(highlightbackground='red',text="Add",command=save,width=36)
+add.grid(row=4,column=1,columnspan=2)
+
+window.mainloop()
